7c7f72796630c70b114c2fa9a17c0666c53e7486
sitncrush <89580762+dvuCodes@users.noreply.github.com>
2023-07-01 20:39:58 +1000

    feat
    -Added functionality to filter buttons in /vans

--- src/pages/Vans/VansList.jsx
+++ src/pages/Vans/VansList.jsx
@@ -1,20 +1,32 @@
 import React, { useContext } from "react"
 import { Context } from "../../Context/Context"
 import VanCard from "../../components/VanCard"
+import { useSearchParams, Link } from "react-router-dom"
 
 const VansList = () => {
   const { vans } = useContext(Context)
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const vanType = searchParams.get("type")
+
+  const displayVans = vanType
+    ? vans.filter((van) => van.type === vanType)
+    : vans
 
   // map over van types to render filter buttons
   const getVanTypes = [...new Set(vans.map((van) => van.type))]
   const renderFilterBtns = getVanTypes.map((type) => (
-    <button className="filter-btn" key={type}>
+    <button
+      className="filter-btn"
+      key={type}
+      onClick={() => setSearchParams({ type: type })}
+    >
       {type}
     </button>
   ))
 
   // map over vans to render them into van cards
-  const renderVanCard = vans.map((van) => {
+  const renderVanCard = displayVans.map((van) => {
     return <VanCard key={van.id} {...van} />
   })
 
@@ -24,7 +36,9 @@ const VansList = () => {
         <h1>Explore our van options</h1>
         <div className="filter-container">
           {renderFilterBtns}
-          <p>Clear Filter</p>
+          <Link to={`?type=`}>
+            <p>Clear Filter</p>
+          </Link>
         </div>
       </div>
       <section className="vans-list-container">{renderVanCard}</section>
